9ef81502ef4629957f1d9b5db166a736a16d1151
chisuhua <chisuhua@gmail.com>
2025-10-15 23:20:21 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+# 简单构建
+#cmake -B build
+#cmake --build build
+
+# 启用详细日志
+#cmake -B build -DENABLE_VERBOSE_LOGGING=ON
+#cmake --build build
+
+# 启用调试日志
+cmake -B build -DENABLE_DEBUG_LOGGING=ON
+cmake --build build
+
+# Debug 模式构建
+#cmake -B build -DCMAKE_BUILD_TYPE=Debug
+#cmake --build build
+
+# 运行测试
+#cmake --build build --target test
+# 或者
+cd build && ctest
