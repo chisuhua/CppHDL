13310a77a333c84d71c87f096d03b454eb60f793
Chi Suhua <chisuhua@gmail.com>
2026-04-13 12:15:46 +0800

    fix(tests): remove dead include in test_cache_simple.cpp
    dynamic_branch_predict.h was deleted during riscv-mini code debt cleanup.
    Remove the stale include and the empty BranchPredictor type-check stub.

--- tests/chlib/test_cache_simple.cpp
+++ tests/chlib/test_cache_simple.cpp
@@ -6,10 +6,8 @@
 #include "catch_amalgamated.hpp"
 #include "cpu/cache/i_cache.h"
 #include "cpu/cache/d_cache.h"
-#include "../examples/riscv-mini/src/dynamic_branch_predict.h"
 
 using namespace chlib;
-using namespace riscv;
 
 TEST_CASE("ICache Type Check", "[cache][icache]") {
     ch::core::context ctx("icache_test");
@@ -22,12 +20,5 @@ TEST_CASE("DCache Type Check", "[cache][dcache]") {
     ch::core::context ctx("dcache_test");
     ch::core::ctx_swap swap(&ctx);
     DCache dcache{nullptr, "dcache"};
-    REQUIRE(true);
-}
-
-TEST_CASE("BranchPredictor Type Check", "[branch][predict]") {
-    ch::core::context ctx("branch_pred_test");
-    ch::core::ctx_swap swap(&ctx);
-    DynamicBranchPredictor predictor{nullptr, "predictor"};
     REQUIRE(true);
 }
